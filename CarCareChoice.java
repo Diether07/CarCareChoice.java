1eddb41e826361f3aaa4a2506c32ad726d23f96e
Dio07 <96104873+Diether07@users.noreply.github.com>
2022-09-30 21:11:17 +1000

    Add files via upload

---
+++ CarCareChoice.java
@@ -0,0 +1,42 @@
+/*Chapter 8 Exercises 3 a
+ *Programmer : Diosdado Cartas 
+ *Date : September 20, 2022
+ *
+ *Write an application for Cody’s Car Care Shop that shows a user a list of available services: 
+ *oil change, tire rotation, battery check, or brake inspection. Allow the user to enter a string that corresponds
+ *to one of the options, and display the option and its price as $25, $22, $15, or $5, accordingly.
+ *Display an error message if the user enters an invalid item. Save the file as CarCareChoice.java.
+ * 
+ * */
+import java.util.Scanner;
+public class CarCareChoice {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String[] list = {"Oil change", "Tire rotation", "Battery check", "Break inspection"};
+		int[] price = {25, 22, 15, 5};
+		String userInput;
+ 
+		System.out.println("*****************Welcome to Cody's Care Care Shop*****************");
+		System.out.println("List of available services: ");
+		for (int i = 0; i < list.length; i++)
+			System.out.print(list[i] + " = $" + price[i] + "\t");
+		//End for loop
+		
+		System.out.println();
+		System.out.println("Enter the type of service that you want : ");
+		userInput = scanner.nextLine();
+		
+		if (userInput.equalsIgnoreCase(list[0])) 
+			System.out.println("You picked " + list[0] + " that will be $" + price[0] );
+		else if (userInput.equalsIgnoreCase(list[1]))
+			System.out.println("You picked " + list[1] + " that will be $" + price[1] );
+		else if (userInput.equalsIgnoreCase(list[2]))
+			System.out.println("You picked " + list[2] + " that will be $" + price[2] );
+		else if (userInput.equalsIgnoreCase(list[3]))
+			System.out.println("You picked " + list[3] + " that will be $" + price[3] );
+		else
+			System.out.println("We don't have that please choose in the available list");
+
+	}//End main
+}//End Class
---
+++ CarChoice2.java
@@ -0,0 +1,72 @@
+/*Chapter 8 Exercises 3 b
+ * Programmer : Diosdado Cartas
+ * Date : September 30, 2022
+ * 
+ * It might not be reasonable to expect users to type long entries such as “oil change” accurately.
+ * Modify the CarCareChoice class so that as long as the user enters the first three characters of a 
+ * service, the choice is considered valid. Save the file as CarCareChoice2.java.
+ * 
+ * */
+import java.util.Scanner;
+public class CarChoice2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String[] list = {"Oil change", "Tire rotation", "Battery check", "Break inspection"};
+		String[] subList = {"Oil", "Tire", "Battery", "Break"};
+		int[] price = {25, 22, 15, 5};
+		String userInput;
+		boolean flag = false;
+		// Revised code and clean
+		System.out.println("*****************Welcome to Cody's Care Care Shop*****************");
+		System.out.println("List of available service: ");
+		for (int i = 0; i < list.length; i++) {
+			System.out.print(list[i] + "\t");
+		}
+		
+		System.out.println();
+		System.out.println("Pick a service that you need: ");
+		userInput = scanner.nextLine();
+		
+		for (int i = 0; i < list.length; i++) {
+			if (list[i].equalsIgnoreCase(userInput) || subList[i].equalsIgnoreCase(userInput)) {
+				System.out.println(list[i] + " cost $" + price[i]);
+				flag = true;
+				break;
+			}
+		}
+		if (!flag) {
+			System.out.println("We don't have that service or check your spelling");
+		}
+		
+		//My Code
+//		
+//		
+// 
+//		System.out.println("*****************Welcome to Cody's Care Care Shop*****************");
+//		System.out.println("List of available services: ");
+//		for (int i = 0; i < list.length; i++)
+//			System.out.print(list[i] + " = $" + price[i] + "\t" );
+//		//End for loop
+//		
+//		System.out.println();
+//		System.out.println("Enter the type of service that you want : ");
+//		userInput = scanner.nextLine();
+//		while (true) 
+//			if (userInput.equalsIgnoreCase(list[0]) || userInput.equalsIgnoreCase(subList[0])) 
+//				System.out.println("You picked " + list[0] + " that will be $" + price[0] );
+//			else if (userInput.equalsIgnoreCase(list[1]) || userInput.equalsIgnoreCase(subList[1]))
+//				System.out.println("You picked " + list[1] + " that will be $" + price[1] );
+//			else if (userInput.equalsIgnoreCase(list[2]) || userInput.equalsIgnoreCase(subList[2]))
+//				System.out.println("You picked " + list[2] + " that will be $" + price[2] );
+//			else if (userInput.equalsIgnoreCase(list[3]) || userInput.equalsIgnoreCase(subList[3]))
+//				System.out.println("You picked " + list[3] + " that will be $" + price[3] );
+//			else 
+//				System.out.println("We don't have that please choose in the available list");
+//				
+//	
+		}
+	
+	}
+
+
